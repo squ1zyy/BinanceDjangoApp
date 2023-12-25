07bc7560672a3e3458ceaa6efeddb809f9a43eaa
Kolihay <tpro83840@gmail.com>
2023-12-25 14:01:57 +0200

    'second'

--- main_app/currency_main/urls.py
+++ main_app/currency_main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
-from views import *
+from .views import *
 
 urlpatterns = [
-    path('', views.main_page, name='main-page'),
+    path('', main_page, name='main-page'),
+    path('currency_graphics/', graphics, name='graphics-page'),
 ]
--- main_app/currency_main/views.py
+++ main_app/currency_main/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from binance.client import Client
+from django.http import JsonResponse
+
+client = Client()
+
+additional_symbols = ['ETHUSDT', 'XRPUSDT', 'LTCUSDT', 'BNBUSDT', 'ADAUSDT', 'DOGEUSDT', 'LINKUSDT', 'DOTUSDT', 'UNIUSDT', 'SOLUSDT']
+
+def update_binance_data(request):
+    data = []
+
+    for symbol in ['BTCUSDT'] + additional_symbols:
+        try:
+            ticker_stats = client.get_ticker(symbol=symbol)
+
+            # Extract relevant data
+            ticker_percentage = ticker_stats['priceChangePercent']
+            ticker_volume = ticker_stats['volume']
+            ticker_high = ticker_stats['highPrice']
+            ticker_low = ticker_stats['lowPrice']
+
+            coin_data = {
+                'symbol': symbol,
+                'high': ticker_high,
+                'low': ticker_low,
+                'percentage': ticker_percentage,
+                'volume': ticker_volume,
+            }
+
+            data.append(coin_data)
+
+        except Exception as e:
+            coin_data = {
+                'symbol': symbol,
+                'error': f"Error fetching data: {e}",
+            }
+            data.append(coin_data)
+
+    context = {
+        'coin_data': data,
+    }
+
+    return render(request, 'main-page.html', context)
+
 
 def main_page(request):
-    return HttpResponse("Hello world!")
+    return render(request,'main-page.html')
+
+def graphics(request):
+    return render(request,'graphics.html')
